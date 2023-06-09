be0994ea9ca04f1b96a95f2a66765d73dab0b7bb
depaula.io <depaula.computacao@gmail.com>
2023-06-09 19:39:22 -0300

    Pacotes

--- first_test/src/edu/depaula/primeirasemana/App.java
+++ first_test/src/edu/depaula/primeirasemana/App.java
@@ -1,3 +1,5 @@
+package edu.depaula.primeirasemana;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Teste de integração com o GitHub!");
